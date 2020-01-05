7b024e056839b6636d04d222ba4de4cd15f5cfdf
kazukinakamuraKN <kajirikajiri@gmail.com>
2020-01-05 14:19:25 +0900

    fix bug

--- docker/api/api/spec/factories/items.rb
+++ docker/api/api/spec/factories/items.rb
@@ -1,9 +1,8 @@
 # spec/factories/items.rb
-require 'faker'
 
 FactoryBot.define do
   factory :item do
-    name { Faker::StarWars.character }
+    name { Faker::Name.name }
     done false
     todo_id nil
   end
